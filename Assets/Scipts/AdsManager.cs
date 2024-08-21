264c604aa1636873ea933a1eafa91d8e7098435b
OrbitalFlag.Studio <121714306+JeromeDassy@users.noreply.github.com>
2024-08-21 19:12:40 +0700

    Initial Commit from local works
    Initial Commit from local works, TODO, optimize list

---
+++ Assets/Scipts/AdsManager.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+
+public class AdsManager : MonoBehaviour
+{
+#if UNITY_ANDROID
+
+    //private void Awake()
+    //{
+    //    Debug.Log("AdMob is Missing");
+    //}
+
+    private BannerView _bannerView;
+    private InterstitialAd _interstitial;
+    private RewardBasedVideoAd _rewardBasedVideoAd;
+
+    public string DeviceID = "BBFF5C1D1E34C26B";
+
+    public void Start()
+    {
+        string appId = "ca-app-pub-2609763721733837~2994240567";
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        // Get singleton reward based video ad reference.
+        _rewardBasedVideoAd = RewardBasedVideoAd.Instance;
+
+        RequestBanner();
+        //RequestInterstitial();
+    }
+
+    private void RequestBanner()
+    {
+        string adUnitId = "ca-app-pub-2609763721733837/3732607165";
+
+        // Create a 320x50 banner.
+        //AdSize adSize = new AdSize(640, 50);
+        _bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        //AdRequest request = new AdRequest.Builder().TagForChildDirectedTreatment(true).Build();
+        //AdRequest request = new AdRequest.Builder().AddTestDevice(DeviceID).Build();     //used for test
+
+        // Load the banner with the request.
+        _bannerView.LoadAd(request);
+    }
+
+    /*
+    private void RequestInterstitial()
+    {
+        string adUnitId = "ca-app-pub-2609763721733837/7568525371";
+
+        // Initialize an InterstitialAd.
+        _interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        //AdRequest request = new AdRequest.Builder().AddTestDevice(DeviceID).Build();
+        //AdRequest request = new AdRequest.Builder().TagForChildDirectedTreatment(true).Build();
+
+        // Load the interstitial with the request.
+        _interstitial.LoadAd(request);
+    }
+
+    public void LoadBasicAd()
+    {
+        RequestInterstitial();
+    }
+
+    public void ShowBasicAd()
+    {
+        if (_interstitial.IsLoaded())
+        {
+            _interstitial.Show();
+        }
+    }
+
+    private void RequestRewardBasedVideo()
+    {
+        string adUnitId = "ca-app-pub-2609763721733837/4343650136";
+
+        _rewardBasedVideoAd.LoadAd(new AdRequest.Builder().Build(), adUnitId);
+        //_rewardBasedVideoAd.LoadAd(new AdRequest.Builder().AddTestDevice(DeviceID).Build(), adUnitId);
+        //_rewardBasedVideoAd.LoadAd(new AdRequest.Builder().TagForChildDirectedTreatment(true).Build(), adUnitId);
+
+        // RewardBasedVideoAd is a singleton, so handlers should only be registered once.
+        _rewardBasedVideoAd.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        _rewardBasedVideoAd.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        _rewardBasedVideoAd.OnAdOpening += HandleRewardBasedVideoOpened;
+        _rewardBasedVideoAd.OnAdStarted += HandleRewardBasedVideoStarted;
+        _rewardBasedVideoAd.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        _rewardBasedVideoAd.OnAdClosed += HandleRewardBasedVideoClosed;
+        _rewardBasedVideoAd.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+    }
+
+    public void LoadRewardAdButton()
+    {
+        RequestRewardBasedVideo();
+    }
+
+    public void ShowRewardAdButton()
+    {
+        ShowRewardBasedAd();
+    }
+
+    private void ShowRewardBasedAd()
+    {
+        if (_rewardBasedVideoAd.IsLoaded())
+        {
+            _rewardBasedVideoAd.Show();
+        }
+    }
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoLoaded event received");
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        //try to reload
+        print("HandleRewardBasedVideoFailedToLoad event received with message: " + args.Message);
+
+    }
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        //Pause the game
+        print("HandleRewardBasedVideoOpened event received");
+    }
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        //Mute The game
+        print("HandleRewardBasedVideoStarted event received");
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        //Back to the game
+        print("HandleRewardBasedVideoClosed event received");
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        //reward the player
+        //string type = args.Type;
+        //double amount = args.Amount;
+        //print("HandleRewardBasedVideoRewarded event received for " + amount.ToString() + " " + type);
+    }
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        print("HandleRewardBasedVideoLeftApplication event received");
+    }
+*/
+#endif
+}
---
+++ Assets/Scipts/AppManager.cs
@@ -0,0 +1,359 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class AppManager : MonoBehaviour
+{
+    public GameObject PrivacyPolicyUI;
+    public GameObject fieldPrefabs;
+    public RectTransform fieldParent;
+
+    public Text bLevel;
+    public Text bState;
+
+    private int PrivacyPolicyInt = 0;
+
+    List<string> headerStingsList = new List<string>();
+    List<string> resultStingsList = new List<string>();
+
+    void Start()
+    {
+        string _deviceModel = "Devise Model";
+        string deviceModel = SystemInfo.deviceModel;
+
+        headerStingsList.Add(_deviceModel);
+        resultStingsList.Add(deviceModel);
+
+        string _deviceName = "Device Name";
+        string deviceName = SystemInfo.deviceName;
+
+        headerStingsList.Add(_deviceName);
+        resultStingsList.Add(deviceName);
+
+        string _deviceType = "Device Type";
+        string deviceType = SystemInfo.deviceType.ToString();
+
+        headerStingsList.Add(_deviceType);
+        resultStingsList.Add(deviceType);
+
+        string _deviceUniqueIdentifier = "Device Unique Identifier";
+        string deviceUniqueIdentifier = SystemInfo.deviceUniqueIdentifier;
+
+        headerStingsList.Add(_deviceUniqueIdentifier);
+        resultStingsList.Add(deviceUniqueIdentifier);
+
+        string _graphicsDeviceID = "Graphics Device ID";
+        string graphicsDeviceID = SystemInfo.graphicsDeviceID.ToString();
+
+        headerStingsList.Add(_graphicsDeviceID);
+        resultStingsList.Add(graphicsDeviceID);
+
+        string _graphicsDeviceName = "Graphics Device Name";
+        string graphicsDeviceName = SystemInfo.graphicsDeviceName;
+
+        headerStingsList.Add(_graphicsDeviceName);
+        resultStingsList.Add(graphicsDeviceName);
+
+        string _graphicsDeviceType = "Graphics Device Type";
+        string graphicsDeviceType = SystemInfo.graphicsDeviceType.ToString();
+
+        headerStingsList.Add(_graphicsDeviceType);
+        resultStingsList.Add(graphicsDeviceType);
+
+        string _graphicsDeviceVendor = "Graphics Device Vendor";
+        string graphicsDeviceVendor = SystemInfo.graphicsDeviceVendor;
+
+        headerStingsList.Add(_graphicsDeviceVendor);
+        resultStingsList.Add(graphicsDeviceVendor);
+
+        string _graphicsDeviceVendorID = "Graphics Device Vendor ID";
+        string graphicsDeviceVendorID = SystemInfo.graphicsDeviceVendorID.ToString();
+
+        headerStingsList.Add(_graphicsDeviceVendorID);
+        resultStingsList.Add(graphicsDeviceVendorID);
+
+        string _graphicsDeviceVersion = "Graphics Device Version";
+        string graphicsDeviceVersion = SystemInfo.graphicsDeviceVersion;
+
+        headerStingsList.Add(_graphicsDeviceVersion);
+        resultStingsList.Add(graphicsDeviceVersion);
+
+        string _graphicsMemorySize = "Graphics Memory Size";
+        string graphicsMemorySize = SystemInfo.graphicsMemorySize.ToString();
+
+        headerStingsList.Add(_graphicsMemorySize);
+        resultStingsList.Add(graphicsMemorySize);
+
+        string _graphicsMultiThreaded = "Graphics Multi Threaded";
+        string graphicsMultiThreaded = SystemInfo.graphicsMultiThreaded.ToString();
+
+        headerStingsList.Add(_graphicsMultiThreaded);
+        resultStingsList.Add(graphicsMultiThreaded);
+
+        string _graphicsShaderLevel = "Graphics Shader Level";
+        string graphicsShaderLevel = SystemInfo.graphicsShaderLevel.ToString();
+
+        headerStingsList.Add(_graphicsShaderLevel);
+        resultStingsList.Add(graphicsShaderLevel);
+
+        string _hasDynamicUniformArrayIndexingInFragmentShaders = "Has Dynamic Uniform Array Indexing In Fragment Shaders";
+        string hasDynamicUniformArrayIndexingInFragmentShaders = SystemInfo.hasDynamicUniformArrayIndexingInFragmentShaders.ToString();
+
+        headerStingsList.Add(_hasDynamicUniformArrayIndexingInFragmentShaders);
+        resultStingsList.Add(hasDynamicUniformArrayIndexingInFragmentShaders);
+
+        string _hasHiddenSurfaceRemovalOnGPU = "Has Hidden Surface Removal On GPU";
+        string hasHiddenSurfaceRemovalOnGPU = SystemInfo.hasHiddenSurfaceRemovalOnGPU.ToString();
+
+        headerStingsList.Add(_hasHiddenSurfaceRemovalOnGPU);
+        resultStingsList.Add(hasHiddenSurfaceRemovalOnGPU);
+
+        string _hasMipMaxLevel = "Has Mip Max Level";
+        string hasMipMaxLevel = SystemInfo.hasMipMaxLevel.ToString();
+
+        headerStingsList.Add(_hasMipMaxLevel);
+        resultStingsList.Add(hasMipMaxLevel);
+
+        string _maxCubemapSize = "Max Cubemap Size";
+        string maxCubemapSize = SystemInfo.maxCubemapSize.ToString();
+
+        headerStingsList.Add(_maxCubemapSize);
+        resultStingsList.Add(maxCubemapSize);
+
+        string _maxTextureSize = "Max Texture Size";
+        string maxTextureSize = SystemInfo.maxTextureSize.ToString();
+
+        headerStingsList.Add(_maxTextureSize);
+        resultStingsList.Add(maxTextureSize);
+
+        string _minConstantBufferOffsetAlignment = "Min Constant Buffer Off set Alignment";
+        string minConstantBufferOffsetAlignment = SystemInfo.minConstantBufferOffsetAlignment.ToString();
+
+        headerStingsList.Add(_minConstantBufferOffsetAlignment);
+        resultStingsList.Add(minConstantBufferOffsetAlignment);
+
+        string _npotSupport = "Non Power of Dot Support";
+        string npotSupport = SystemInfo.npotSupport.ToString();
+
+        headerStingsList.Add(_npotSupport);
+        resultStingsList.Add(npotSupport);
+
+        string _operatingSystem = "operating System";
+        string operatingSystem = SystemInfo.operatingSystem;
+
+        headerStingsList.Add(_operatingSystem);
+        resultStingsList.Add(operatingSystem);
+
+        string _operatingSystemFamily = "Operating System Family";
+        string operatingSystemFamily = SystemInfo.operatingSystemFamily.ToString();
+
+        headerStingsList.Add(_operatingSystemFamily);
+        resultStingsList.Add(operatingSystemFamily);
+
+        string _processorCount = "Processor Count";
+        string processorCount = SystemInfo.processorCount.ToString();
+
+        headerStingsList.Add(_processorCount);
+        resultStingsList.Add(processorCount);
+
+        string _processorFrequency = "Processor Frequency";
+        string processorFrequency = SystemInfo.processorFrequency.ToString();
+
+        headerStingsList.Add(_processorFrequency);
+        resultStingsList.Add(processorFrequency);
+
+        string _processorType = "Processor Type";
+        string processorType = SystemInfo.processorType;
+
+        headerStingsList.Add(_processorType);
+        resultStingsList.Add(processorType);
+
+        string _supportedRenderTargetCount = "Supported Render Target Count";
+        string supportedRenderTargetCount = SystemInfo.supportedRenderTargetCount.ToString();
+
+        headerStingsList.Add(_supportedRenderTargetCount);
+        resultStingsList.Add(supportedRenderTargetCount);
+
+        string _supports2DArrayTextures = "Supports 2D Array Textures";
+        string supports2DArrayTextures = SystemInfo.supports2DArrayTextures.ToString();
+
+        headerStingsList.Add(_supports2DArrayTextures);
+        resultStingsList.Add(supports2DArrayTextures);
+        
+        string _supports32bitsIndexBuffer = "Supports 32bits Index Buffer";
+        string supports32bitsIndexBuffer = SystemInfo.supports32bitsIndexBuffer.ToString();
+
+        headerStingsList.Add(_supports32bitsIndexBuffer);
+        resultStingsList.Add(supports32bitsIndexBuffer);
+
+        string _supports3DRenderTextures = "Supports 3D Render Textures";
+        string supports3DRenderTextures = SystemInfo.supports3DRenderTextures.ToString();
+
+        headerStingsList.Add(_supports3DRenderTextures);
+        resultStingsList.Add(supports3DRenderTextures);
+
+        string _supports3DTextures = "Supports 3D Textures";
+        string supports3DTextures = SystemInfo.supports3DTextures.ToString();
+
+        headerStingsList.Add(_supports3DTextures);
+        resultStingsList.Add(supports3DTextures);
+
+        string _supportsAccelerometer = "Supports Accelerometer";
+        string supportsAccelerometer = SystemInfo.supportsAccelerometer.ToString();
+
+        headerStingsList.Add(_supportsAccelerometer);
+        resultStingsList.Add(supportsAccelerometer);
+
+        string _supportsAsyncCompute = "Supports Async Compute";
+        string supportsAsyncCompute = SystemInfo.supportsAsyncCompute.ToString();
+
+        headerStingsList.Add(_supportsAsyncCompute);
+        resultStingsList.Add(supportsAsyncCompute);
+
+        string _supportsAsyncGPUReadback = "Supports Async GPU Readback";
+        string supportsAsyncGPUReadback = SystemInfo.supportsAsyncGPUReadback.ToString();
+
+        headerStingsList.Add(_supportsAsyncGPUReadback);
+        resultStingsList.Add(supportsAsyncGPUReadback);
+
+        string _supportsAudio = "Supports Audio";
+        string supportsAudio = SystemInfo.supportsAudio.ToString();
+
+        headerStingsList.Add(_supportsAudio);
+        resultStingsList.Add(supportsAudio);
+
+        string _supportsComputeShaders = "Supports Compute Shaders";
+        string supportsComputeShaders = SystemInfo.supportsComputeShaders.ToString();
+
+        headerStingsList.Add(_supportsComputeShaders);
+        resultStingsList.Add(supportsComputeShaders);
+
+        string _supportsCubemapArrayTextures = "Supports Cubemap Array Textures";
+        string supportsCubemapArrayTextures = SystemInfo.supportsCubemapArrayTextures.ToString();
+
+        headerStingsList.Add(_supportsCubemapArrayTextures);
+        resultStingsList.Add(supportsCubemapArrayTextures);
+
+        string _supportsGyroscope = "Supports Gyroscope";
+        string supportsGyroscope = SystemInfo.supportsGyroscope.ToString();
+
+        headerStingsList.Add(_supportsGyroscope);
+        resultStingsList.Add(supportsGyroscope);
+        
+        string _supportsHardwareQuadTopology = "Supports Hardware Quad Topology";
+        string supportsHardwareQuadTopology = SystemInfo.supportsHardwareQuadTopology.ToString();
+
+        headerStingsList.Add(_supportsHardwareQuadTopology);
+        resultStingsList.Add(supportsHardwareQuadTopology);
+
+        string _supportsInstancing = "Supports Instancing";
+        string supportsInstancing = SystemInfo.supportsInstancing.ToString();
+
+        headerStingsList.Add(_supportsInstancing);
+        resultStingsList.Add(supportsInstancing);
+
+        string _supportsLocationService = "Supports Location Service";
+        string supportsLocationService = SystemInfo.supportsLocationService.ToString();
+
+        headerStingsList.Add(_supportsLocationService);
+        resultStingsList.Add(supportsLocationService);
+
+        string _supportsMipStreaming = "Supports Mip Streaming";
+        string supportsMipStreaming = SystemInfo.supportsMipStreaming.ToString();
+
+        headerStingsList.Add(_supportsMipStreaming);
+        resultStingsList.Add(supportsMipStreaming);
+
+        string _supportsMotionVectors = "Supports Motion Vectors";
+        string supportsMotionVectors = SystemInfo.supportsMotionVectors.ToString();
+
+        headerStingsList.Add(_supportsMotionVectors);
+        resultStingsList.Add(supportsMotionVectors);
+
+        string _supportsMultisampleAutoResolve = "Supports Multisample Auto Resolve";
+        string supportsMultisampleAutoResolve = SystemInfo.supportsMultisampleAutoResolve.ToString();
+
+        headerStingsList.Add(_supportsMultisampleAutoResolve);
+        resultStingsList.Add(supportsMultisampleAutoResolve);
+
+        string _supportsMultisampledTextures = "Supports Multisampled Textures";
+        string supportsMultisampledTextures = SystemInfo.supportsMultisampledTextures.ToString();
+
+        headerStingsList.Add(_supportsMultisampledTextures);
+        resultStingsList.Add(supportsMultisampledTextures);
+
+        string _supportsRawShadowDepthSampling = "Supports Raw Shadow Depth Sampling";
+        string supportsRawShadowDepthSampling = SystemInfo.supportsRawShadowDepthSampling.ToString();
+
+        headerStingsList.Add(_supportsRawShadowDepthSampling);
+        resultStingsList.Add(supportsRawShadowDepthSampling);
+
+        string _supportsSetConstantBuffer = "Supports Set Constant Buffer";
+        string supportsSetConstantBuffer = SystemInfo.supportsSetConstantBuffer.ToString();
+
+        headerStingsList.Add(_supportsSetConstantBuffer);
+        resultStingsList.Add(supportsSetConstantBuffer);
+
+        string _supportsShadows = "Supports Shadows";
+        string supportsShadows = SystemInfo.supportsShadows.ToString();
+
+        headerStingsList.Add(_supportsShadows);
+        resultStingsList.Add(supportsShadows);
+
+        string _supportsSparseTextures = "Supports Sparse Textures";
+        string supportsSparseTextures = SystemInfo.supportsSparseTextures.ToString();
+
+        headerStingsList.Add(_supportsSparseTextures);
+        resultStingsList.Add(supportsSparseTextures);
+
+        string _supportsVibration = "Supports Vibration";
+        string supportsVibration = SystemInfo.supportsVibration.ToString();
+
+        headerStingsList.Add(_supportsVibration);
+        resultStingsList.Add(supportsVibration);
+
+        string _systemMemorySize = "System Memory Size";
+        string systemMemorySize = SystemInfo.systemMemorySize.ToString();
+
+        headerStingsList.Add(_systemMemorySize);
+        resultStingsList.Add(systemMemorySize);
+
+        GenerateFeild();
+    }
+
+    void GenerateFeild()
+    {
+        for (int i = 0; i < headerStingsList.Count; i++)
+        {
+            GameObject instantiatedField = Instantiate(fieldPrefabs, (fieldParent.gameObject.GetComponent<RectTransform>()) );
+
+            //DarkBackground/ HeaderInfoText
+            //DarkBackground/ InfosValue
+
+            Transform tHeader = instantiatedField.transform.Find("HeaderInfoText");
+            Transform tInfo = instantiatedField.transform.Find("InfosValue");
+
+            Text tHeaderText = tHeader.GetComponent<Text>();
+            Text tInfoText = tInfo.GetComponent<Text>();
+
+            tHeaderText.text = headerStingsList[i];
+            tInfoText.text = resultStingsList[i];
+        }
+
+        PrivacyPolicyInt = PlayerPrefs.GetInt("PrivacyPolicyMessage");
+
+        if (PrivacyPolicyInt == 0)
+        {
+            PrivacyPolicyUI.SetActive(true);
+            PlayerPrefs.SetInt("PrivacyPolicyMessage", 1);
+        }
+    }
+
+    void Update()
+    {
+        string batteryLevel = (SystemInfo.batteryLevel * 100f).ToString();
+        bLevel.text = batteryLevel + " %";
+        string batteryStatus = SystemInfo.batteryStatus.ToString();
+        bState.text = batteryStatus;
+    }
+}
